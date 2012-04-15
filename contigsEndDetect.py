d20f51feb7ae3e3221c7ce8aba185b4a731519c5
mohit sharma <mohit.786.sharma@gmail.com>
2012-04-15 14:40:24 -0500

    modified to detect flipped end overlaps too

--- contigsEndDetect.py
+++ contigsEndDetect.py
@@ -1,18 +1,18 @@
 import sys
 
 """from mummer output based on start and end pts, identify the contigs that
-overlapped at end append '1' if yes and '0' other wise"""
+overlapped at end append 'S/E/SF/EF' if yes and '0' other wise"""
 
 
 def detectIfEndAlign(ipFileName, outFileName):
     #matched start and end columns of query and reference
     #TODO: check if last index is length - 1 in base
-    queryStartCol = 3
-    queryEndCol = 4
+    queryMatchStartCol = 3
+    queryMatchEndCol = 4
     queryLengthCol = 9
     
-    refStartCol = 1
-    refEndCol = 2
+    refMatchStartCol = 1
+    refMatchEndCol = 2
     refLengthCol = 8
     
     ipFile = open(ipFileName, 'r')
@@ -22,31 +22,47 @@ def detectIfEndAlign(ipFileName, outFileName):
             line = line.rstrip('\n')
             lineStrs = line.split()
             #get start and end, -1 as zero indexing
-            queryStart = int(lineStrs[queryStartCol-1])
-            queryEnd = int(lineStrs[queryEndCol-1])
+            queryMatchStart = int(lineStrs[queryMatchStartCol-1])
+            queryMatchEnd = int(lineStrs[queryMatchEndCol-1])
             queryLength = int(lineStrs[queryLengthCol - 1])
             
-            refStart = int(lineStrs[refStartCol - 1])
-            refEnd = int(lineStrs[refStartCol - 1])
+            refMatchStart = int(lineStrs[refMatchStartCol - 1])
+            refMatchEnd = int(lineStrs[refMatchEndCol - 1])
             refLength = int(lineStrs[refLengthCol - 1])
 
             queryEndRefStartOvrlap = False
             queryStartRefEndOvrlap = False
+            queryStartRefStartFlipped = False
+            queryEndRefEndFlipped = False
+            
             #identify if query end overlaps reference start
-            #TODO: queryLength - 1 check
-            if queryEnd == (queryLength) and refStart == 1:#0
+            if queryMatchEnd == (queryLength) and refMatchStart == 1:#0
                 #query end and reference start overlaps
                 queryEndRefStartOvrlap = True
+                
             #identify if reference end overlaps query start
-            #TODO: refLength - 1 check
-            if queryStart == 1 and refEnd == (refLength):
+            if queryMatchStart == 1 and refMatchEnd == (refLength):
                 #query end and reference start overlaps
                 queryStartRefEndOvrlap = True
+            
+            if queryMatchEnd == 1 and refMatchStart == 1:
+                #query start and reference start overlaps with flipped query
+                queryStartRefStartFlipped = True
+
+            if queryMatchStart == queryLength and refMatchEnd == refLength:
+                #query end and reference end overlaps with flipped query
+                queryEndRefEndFlipped = True
+
             endOverlapType='0'
             if queryEndRefStartOvrlap:
                 endOverlapType='S'
             if queryStartRefEndOvrlap:
                 endOverlapType='E'
+            if queryStartRefStartFlipped:
+                endOverlapType = 'SF'
+            if queryEndRefEndFlipped:
+                endOverlapType = 'EF'
+
             lineStrs.append(endOverlapType)
             line = '\t'.join(lineStrs)
             
