d5ba1048072e4cd51fa8260107f3229cfb7f6ce6
mohit sharma <mohit.786.sharma@gmail.com>
2012-04-18 14:54:43 -0500

    removed infinite loop bug

--- contigsExtractMatchingRegion.py
+++ contigsExtractMatchingRegion.py
@@ -31,10 +31,13 @@ def extractMatchedRegion(matchingDict, contigsFastaFileName, opFileName):
                     opFile.write(header)
                     opFile.write('\n')
                     opFile.write(matched)
-                    opFile.write('\n')
+                    opFile.write('\n')                    
                     #don't need this key, can delete it
                     del matchingDict[header]
-                    line = contigsFile.readline()
+                    if len(matchingDict) == 0:
+                        #all the dict is processed exit the loop
+                        break
+            line = contigsFile.readline()
         contigsFile.close()
         opFile.close()
     except IOError as (errno, strerror):
@@ -58,9 +61,73 @@ def createMatchingDict(matchFileName):
             start = columns[startColNo]
             end = columns[endColNo]
             matchingDict[header] = {startKey:start, endKey:end}
+        matchFile.close()
     except IOError as (errno, strerror):
         print "I/O error({0}): {1}".format(errno, strerror)
     return matchingDict
+
+def batchExtractMatchingRegion(matchingDict, contigsFileName, opFile):
+    try:
+        contigsFile = open(contigsFastaFileName, 'r')
+        line=contigsFile.readline()
+        while line:
+            if line.startswith('>'):
+                #detected header
+                header = line.lstrip('>').rstrip('\n').split()[0]
+                if header in matchingDict:
+                    #current contig one of matching contig
+                    #extract the matching region from it
+                    start = matchingDict[header][0]
+                    end = matchingDict[header][1]
+                    if end < start:
+                        #contigs matching in reverse direction
+                        start, end = end, start
+                    contigs = contigsFile.readline().rstrip('\n')
+                    matched = contigs[start:end+1]
+                    opFile.write('>')
+                    opFile.write(header)
+                    opFile.write('\n')
+                    opFile.write(matched)
+                    opFile.write('\n')
+                    opFile.flush()
+                    #don't need this key, can delete it
+                    del matchingDict[header]
+                    if len(matchingDict) == 0:
+                        #all the batch is processed exit the loop
+                        break
+            line = contigsFile.readline()
+        contigsFile.close()
+    except IOError as (errno, strerror):
+        print "I/O error({0}): {1}".format(errno, strerror)
+
+
+
+""" huge file so lets do batch extraction"""
+def doBatchExtraction(matchFileName, contigsFileName, opFileName):
+    matchingDict = {}
+    headerColNo = 1 -1 #-1 as zero based indexing
+    startColNo = 2 -1
+    endColNo = 3 -1
+    #size of dict to be created at one time and send fwd for processing
+    batchLength = 1000 
+    try:
+        matchFile = open(matchFileName, 'r')
+        opFile = open(opFileName, 'w')
+        header = matchFile.readline()
+        for line in matchFile:
+            columns = line.split()
+            header = columns[headerColNo]
+            start = columns[startColNo]
+            end = columns[endColNo]
+            matchingDict[header] = {[int(start), int(end)]}
+            if len(matchingDict) >= batchLength:
+                #process these batch and empty the dict
+                somethingTODO = ''
+        matchFile.close()
+        opFileName.close()
+    except IOError as (errno, strerror):
+        print "I/O error({0}): {1}".format(errno, strerror)
+
 
 def main():
     if len(sys.argv) >= 3:
