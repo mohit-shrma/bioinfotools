0541edbd9d89c820c351d65bda7c902ce287c494
mohit sharma <mohit.786.sharma@gmail.com>
2012-05-22 22:22:20 -0500

    corrected for combining fastQ's then performing computation

--- avgQScores/avgQScoreMulti.py
+++ avgQScores/avgQScoreMulti.py
@@ -12,36 +12,66 @@ def getAbsPath(dir):
     return absDir
 
 
-""" identify all the fastq's inside a folder,
-parentDir/samplesDir{1,2,3...16}/<xyz.{fastq|fastq.gz}>"""
+#concatenate all the fastQ's inside a sample dir
+def combineFastQs(sampleDir):
 
-def getAllFastqsPath(samplesDir):
+    #get the sample dir name
+    dirName = sampleDir.split('/')[-1]
 
-    fastQsFilePaths = []
-    
-    samplsDirContents = os.listdir(samplesDir)
+    #combined output file path
+    combinedFilePath = getAbsPath(sampleDir) + dirName\
+        + '.fastq'
+
+    #concatenate all fastq's inside to fastqpath
+    retcode = -1
+    try:
+        retcode = call(["cat ", "*.fastq", " > " + combinedFilePath])
+        if retcode == 0:
+            #successfully concatenated fastq
+            retcode = 1
+        else:
+            #error found in concatenation
+            print "fastqCombine error: ", sampleDir, retcode
+            retcode = -1
+    except OSError, e:
+        print >>sys.stderr, "Execution failed: ", e
+        print "fastqCombine error: ", sampleDir
+        retcode = -1
+    return retcode
+
+
+#return all sample dir paths in a list
+def allSampleDirsPath(sampleDirsParent):
+
+    #get all contents inside saple directory
+    samplesDirContents = os.listdir(sampleDirsParent)
     allSamplesDir = []
-
+    
     #get list of samples directory inside the passed dir
     for name in samplsDirContents:
         contentPath = os.path.join(samplesDir, name)
         if os.path.isdir(contentPath):
             allSamplesDir.append(contentPath)
+    return allSamplesDir
 
+
+""" identify all the fastq's inside a folder,
+parentDir/samplesDir{1,2,3...16}/<xyz.{fastq|fastq.gz}>"""
+
+def getAllFastqsPath(samplesDir):
+    fastQsFilePaths = []
+    #get list of samples directory inside the passed dir
+    allSamplesDir = allSampleDirsPath(samplesDir)
     #find all fastq's after
     for sampleDir in allSamplesDir:
-
         sampleDirContents = os.listdir(sampleDir)
-
         for name in sampleDirContents:
-            
             if name.endswith('.fastq'):
                 if name+'.gz' not in sampleDirContents:
                     fastQsFilePaths.append(os.path.join(sampleDir, name))
             elif name.endswith('.fastq.gz'):
                 #either fastq or fastq compressed file
                 fastQsFilePaths.append(os.path.join(sampleDir, name))
-                
     return fastQsFilePaths
 
 #return directory containing file and file name without extension
@@ -88,7 +118,39 @@ def qscoreWorker((fastQPath, qScoreFilePath, qScorePerlScript)):
 
     #flush the output stream
     sys.stdout.flush()
+    retcode = computeQScore(qScorePerlScript, fastQPath, qScoreFilePath,\
+                            seqAvgsFilePath, seqAvgScorePath)
+    return retcode
+
+#worker that combine fastQ's inside samples and compute Qscore on combined fastq result
+def workerToCombineAndCompute((sampleDir, qScoreFilePath, qScorePerlScript)):
+
+    print sampleDir + 'worker'
     
+    #combine all fastq's inside sample directory
+    retcode = combineFastQs(sampleDir)
+
+    if retcode == -1:
+        return False
+
+    sampleDirName = os.path.basename(sampleDir.rstrip('/'))
+    combinedFastQPath = os.path.join(sampleDir, sampleDirName + '.fastq')
+
+    #output file paths to be passed to perl script
+    seqAvgsFilePath = os.path.join(sampleDir, sampleDirName +  '_seqavgs.txt')
+    seqAvgScorePath = os.path.join(sampleDir, sampleDirName +  '_avgscore.txt')
+
+    #compute q score for combinedFastQ file
+    retcode = computeQScore(qScorePerlScript, combinedFastQPath, qScoreFilePath,\
+                                seqAvgsFilePath, seqAvgScorePath)
+    if retcode == 1:
+        return True
+    else:
+        return False
+
+#compute the q score by calling the perl script with passed arguments
+def computeQScore(qScorePerlScript, fastQPath, qScoreFilePath,\
+                       seqAvgsFilePath, seqAvgScorePath):
     #call perl script for calculation on passed fastq
     #retcode = call("perl" + qScorePerlScript
     #    + " fastQPath qScoreFilePath seqAvgsFilePath seqAvgScorePath ",
@@ -96,29 +158,32 @@ def qscoreWorker((fastQPath, qScoreFilePath, qScorePerlScript)):
     
     try: 
         retcode = call(["perl", qScorePerlScript, fastQPath, qScoreFilePath,\
-                       seqAvgsFilePath, seqAvgScorePath])
+                            seqAvgsFilePath, seqAvgScorePath])
         if retcode == 0:
             #passed
             print fastQPath + " evaluated"
-            return 1
+            retcode =  1
         else:
             #failed
-            print fastQPath + " evaluation failed"
+            print fastQPath + " evaluation failed", retcode
+            retcode = -1
     except OSError, e:
-        print >>sys.stderr, "Execution failed: ", e
+        print >>sys.stderr, "Execution failed: ", e, fastQPath
+        retcode = -1
+    return retcode
+        
 
-    return  -1    
+#prepare child workers to work for each directory
+def prepAndRunWorkers(allSamplesDir, qScoreFilePath, qScorePerlScript):
 
-def prepAndRunWorkers(fastQPaths, qScoreFilePath, qScorePerlScript):
-
-    pool = Pool(processes=min(len(fastQPaths), 32))
+    pool = Pool(processes=min(len(allSamplesDir), 32))
     workersArgs = []
     
     #prepare argument for each worker
-    for fastQPath in fastQPaths:
-        workersArgs.append((fastQPath, qScoreFilePath, qScorePerlScript))
+    for sampleDir in allSamplesDir:
+        workersArgs.append((sampleDir, qScoreFilePath, qScorePerlScript))
     print "calling pool.map"
-    results = pool.map(qscoreWorker, workersArgs)
+    results = pool.map(workerToCombineAndCompute, workersArgs)
     pool.close()
     pool.join()
 
@@ -129,7 +194,6 @@ def main():
     logger = multiprocessing.log_to_stderr()
     logger.setLevel(multiprocessing.SUBDEBUG)
 
-    
     if len(sys.argv) >= 3:
         #directory containing samples which contains fastq
         samplesDir = getAbsPath(sys.argv[1])
@@ -140,14 +204,14 @@ def main():
         #perl script for qscore calculation
         qScorePerlScript = getAbsPath(sys.argv[3]).rstrip('/')
         
-        #fastQs paths
-        fastQFilePaths = getAllFastqsPath(samplesDir)
-
-        #display fastQFilePaths contents
-        print fastQFilePaths, len(fastQFilePaths)
+        #get all sample directory inside samplesDir
+        allSamplesDir = allSampleDirsPath(samplesDir)
         
         #call child workers to do the job
-        prepAndRunWorkers(fastQFilePaths, qScoreFile, qScorePerlScript)
+        results = prepAndRunWorkers(allSamplesDir, qScoreFile, qScorePerlScript)
+
+        #print final results
+        print "avgQScoreComputation success: ", all(results)
     else:
         print 'err: files missing'
 
