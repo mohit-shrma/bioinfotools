6b198139afd95e1c1b84b7493af45c6f68aba49b
mohit sharma <mohit.786.sharma@gmail.com>
2012-06-19 17:55:15 -0500

    added fastqs processing only

---
+++ multiSNPs/multiBamsFromFastQs.py
@@ -0,0 +1,83 @@
+import sys
+import os
+import parallelSNPsFinder
+from multiprocessing import Pool
+import multiprocessing, logging
+
+import workerForBam
+
+def getAbsPath(dir):
+    absDir = os.path.abspath(dir)
+    if not absDir.endswith('/'):
+        absDir += '/'
+    return absDir
+
+
+def writeCombineFastqJobs(outDir, fastqDir, fastaPath, lockDirPath):
+    combinedBAMJobsName = 'combinedBAMJob.jobs'
+    combinedBAMJobsPath = os.path.join(outDir, combinedBAMJobsName)
+    tools = workerForBam.getToolsDict()
+    #contained all fastas against which to map the fastqs
+    fastaFilePaths = []    
+    #fastaPath contains all .fasta inside a dir with same name as fasta
+    #get all fasta name without ext ".fasta" in a list
+    fastaDirs = workerForBam.getAllFastas(fastaPath)
+    for fastaDir in fastaDirs:
+        #get fasta File Path        
+        fastaFilePath = fastaPath + fastaDir + "/" + fastaDir + ".fasta"
+        fastaFilePaths.append(fastaFilePath)
+    print 'fastaFilePaths: ', fastaFilePaths
+    with open(combinedBAMJobsPath, 'w') as combinedBAMJobsFile:
+        dirContents = os.listdir(fastqDir)
+        for fileName in dirContents:
+            fastqPath = os.path.join(fastqDir, fileName)
+            if os.path.isfile(fastqPath) and\
+                    fileName.endswith('fastq'):
+                for fastaFilePath in fastaFilePaths:
+                    workerForBam.writeJob(combinedBAMJobsFile, fastaFilePath,\
+                                              fastqPath, lockDirPath, tools)
+    return combinedBAMJobsPath
+
+
+                    
+def main():
+
+    logger = multiprocessing.log_to_stderr()
+    logger.setLevel(multiprocessing.SUBDEBUG)
+
+    if len(sys.argv) >= 4:
+        #directory containing fastq library
+        fastqsDir = getAbsPath(sys.argv[1])
+        
+        #directory containing other directories with fasta names
+        fastaDir = getAbsPath(sys.argv[2])
+
+        #directory containing file locks
+        lockDirPath = getAbsPath(sys.argv[3])
+        
+        #directory containing temp output -> fastQ's, jobsFile 
+        outDir = getAbsPath(sys.argv[4])
+
+        #write all fastq's processing in job file
+        combineJobPath = writeCombineFastqJobs(outDir, fastqsDir, fastaDir,\
+                                                   lockDirPath)
+
+
+        tools = workerForBam.getToolsDict()
+        retcode = workerForBam.callParallelDrone(combineJobPath,\
+                                                     tools['PARALLEL_DRONE'])
+
+        if retcode != 0:
+            #error occured while calling parallel drone
+            print "parallel drone erred, in executing combined jobs"
+            return -1
+        
+        #now for all scaffolds combined bams and look for SNPs
+        #parallelSNPsFinder.snpsFinder(fastaDir, outDir, locksDir)
+        
+    else:
+        print 'err: files missing'
+
+if __name__ == '__main__':
+    main()
+
--- multiSNPs/workerForBam.py
+++ multiSNPs/workerForBam.py
@@ -31,8 +31,7 @@ def getToolsDict():
     tools['PICARD_TOOLS'] ="/home/koronis/mohit/programs/picard-tools-1.68"
     tools['GENOME_ANALYSIS_TK_JAR'] = "/home/koronis/mohit/programs/GenomeAnalysisTK/GenomeAnalysisTK.jar"
     tools['VARSCAN_JAR'] = "/home/koronis/mohit/programs/varscan/VarScan.v2.2.10.jar"
-    #tools['PARALLEL_DRONE'] = "mpiexec_mpt -np 96 /home/koronis/mohit/programs/Drone/Drone "
-    tools['PARALLEL_DRONE'] = "mpiexec_mpt -np 48 /home/koronis/mohit/programs/Drone/Drone "
+    tools['PARALLEL_DRONE'] = "mpiexec_mpt -np 12 /home/koronis/mohit/programs/Drone/Drone "
     return tools
 
 #a dictionary of all possible extensions
