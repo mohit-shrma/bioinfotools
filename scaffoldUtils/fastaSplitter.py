bda8046c5aafbcb5d4ceecacf7934ded1020a9e8
mohit sharma <mohit.786.sharma@gmail.com>
2012-07-11 13:36:43 -0500

    modified cutofflength for splitting fasta

--- scaffoldUtils/fastaSplitter.py
+++ scaffoldUtils/fastaSplitter.py
@@ -1,29 +1,49 @@
 import sys
 import os
 
-""" takes input scaffold file, outputs all FASTA sequences in corresponding
-seaprate file named after header into output directory"""
-def splitFasta(scaffsFileName, outDir):
-    with open(scaffsFileName, 'r') as scaffsFile:
-        tempOutFile = None
-        try:
-            for line in scaffsFile:
-                if line.startswith('>'):
-                    #found header
-                    newFileName = (line.rstrip('\n'))[1:]+'.fasta'
-                    tempOutFile = open(os.path.join(outDir, newFileName), 'w')
-                    tempOutFile.write(line)
-                else:
-                    #found sequence
-                    tempOutFile.write(line)
-                    tempOutFile.close()
-        except IOError as e:
-            print "I/O error({0}): {1}".format(e.errno, e.strerror)
 
+""" takes input scaffold file, outputs all FASTA sequences in corresponding
+seaprate file named after header into output directory,
+optional cutOffLen can be provided for sequences to be ignored"""
+def splitFasta(scaffsFileName, outDir, cutOffLen = None):
+    with open(scaffsFileName, 'r') as scaffsFile:
+        header = None
+        for line in scaffsFile:
+            if line.startswith('>'):
+                #found header
+                header = line.rstrip('\n')
+            else:
+                #found sequence
+                if cutOffLen is not None:
+                    if (len(line) - 1) >= cutOffLen: # -1 for '\n'
+                        #sequence length is gr8r than cutoff len
+                        #write out corresponding sequence & header
+                        writeOutSeq(header, line.rstrip('\n'), outDir)
+                else:
+                    #write out corresponding sequence & header
+                    writeOutSeq(header, line.rstrip('\n'), outDir)
+
+                    
+
+""" write the header and sequence into a file in outDir, e.g. if header='>SN1',
+then it creates a file with name SN1.fasta and write out the sequence """
+def writeOutSeq(header, sequence, outDir):
+    fileName = header[1:] + '.fasta'
+    with open(os.path.join(outDir, fileName), 'w') as tempOutFile:
+        #write header
+        tempOutFile.write(header + '\n')
+        #write sequence
+        tempOutFile.write(sequence + '\n')
+            
 
 def main():
     argLen = len(sys.argv)
-    if argLen >= 3:
+    if argLen >= 4:
+        scaffsFileName = sys.argv[1]
+        outDir = sys.argv[2]
+        cutOffLen = int(sys.argv[3])
+        splitFasta(scaffsFileName, outDir, cutOffLen)
+    elif argLen >= 3:
         scaffsFileName = sys.argv[1]
         outDir = sys.argv[2]
         splitFasta(scaffsFileName, outDir)
