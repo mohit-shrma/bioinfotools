0db1f587d230301b7d4b07097bd18f3301429907
mohit sharma <mohit.786.sharma@gmail.com>
2012-10-03 18:27:30 -0500

    update pile up reads

--- readUtils/pileUpStartPosCount.py
+++ readUtils/pileUpStartPosCount.py
@@ -3,22 +3,73 @@ import os
 
 class SAMConsts:
 
+    #position col
+    POSITION_COL = 2
+    
     #number of reads col
     NUM_READS_COL = 3
 
+    #reads alignment pos style col
+    ALIGN_POS_STYLE_COL = 4
+
+    #reads start style
+    START_STYLE = '^F'
+    START_STYLE_FIRST = '^'
+    START_STYLE_SECOND = 'F'
+
+    #reads end style
+    END_STYLE = '$'
+
     
 
-
 def countReadsAtEachPosition(pileUpFileName):
+
     with open(pileUpFileName, 'r') as pileUpFile:
-        for pileUp in pileUpFile:
+
+        print 'Position\tStart Count\tEnd Count\n' 
+
+        for line in pileUpFile:
+
+            cols = line.split()
+
+            #position on reference
+            position = cols[SAMConsts.POSITION_COL]
+
+            #reads aligned style on this position
+            alignReadsStyle = cols[SAMConsts.ALIGN_POS_STYLE_COL]
+
+            #count of reads aligned with their start and end
+            startCount = 0
+            endCount = 0
+
+            lastStart = False
+
+            for readStyle in alignReadsStyle:
+                if alignReadsStyle[readStyle] == SAMConsts.START_STYLE_FIRST:
+                    #found beginning character of start
+                    lastStart = True
+                elif lastStart and \
+                        alignReadsStyle[readStyle] == SAMConsts.START_STYLE_SECOND:
+                    startCount += 1
+                    lastStart = False
+                elif alignReadsStyle[readStyle] == SAMConsts.END_STYLE:
+                    endCount += 1
+                    lastStart = False
+                else:
+                    lastStart = False
+                    
+            print position + '\t' + str(startCount) + '\t' + endCount + '\n'
+            
+                    
+                    
             
 
 def main():
     argLen = len(sys.argv)
     if argLen >= 1:
         #passed pile up file
-        pass
+        pileupFileName = sys.argv[1]
+        countReadsAtEachPosition(pileupFileName)
     else:
         print 'err: less num of args passed'
 
